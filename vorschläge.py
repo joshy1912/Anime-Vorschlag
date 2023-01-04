03cf1ecabcb2940f089474727275cf80c2279a49
joshy1912 <99416967+joshy1912@users.noreply.github.com>
2023-01-04 11:21:25 +0100

    Add files via upload

---
+++ vorschläge.py
@@ -0,0 +1,107 @@
+import time as ti
+import webbrowser as web
+print("Welches Genre willst du vorgeschlagen bekommen?")
+print("action")
+print("Abenteuer")
+print("actionodrama")
+print("Actionkomödie")
+print("Alltagsleben")
+print("Alltagsdrama")
+print ("Boys love")
+print("Drama")
+print ("ecchi")
+print ("engsub")
+print ("erotik")
+print ("fantasy")
+print("Fighting-shounen")
+print ("ganbatte")
+print("geistergeschichten")
+print ("ger")
+print ("gersub")
+print ("harem")
+print ("horror")
+print ("komödie")
+print ("krimi")
+print ("liebesdrama")
+print ("magical girl")
+print ("mecha")
+print ("Mystery")
+print("Nonsense-komödie")
+print ("Psychodrama")
+print("Romatische komödie")
+print ("Romanze")
+print ("scifi")
+print ("sport")
+print ("thriller")
+print ("yuri")
+print ("Gewalt")
+User_Input=input()
+
+if User_Input == 'action':
+    web.open ("https://aniworld.to/genre/action")
+elif User_Input == 'Abenteuer':
+    web.open ("https://aniworld.to/genre/abenteuer")
+elif User_Input == 'actiondrama':
+    web.open ("https://aniworld.to/genre/actiondrama")
+elif User_Input == 'actionkomödie':
+    web.open ("https://aniworld.to/genre/actionkomoedie")
+elif User_Input == 'Alltagsleben':
+    web.open ("https://aniworld.to/genre/alltagsleben")
+elif User_Input == 'Alltagsdrama':
+    web.open ("https://aniworld.to/genre/alltagsdrama")
+elif User_Input == 'boys love':
+    web.open ("https://aniworld.to/genre/boys-love")
+elif User_Input== 'drama':
+    web.open ("https://aniworld.to/genre/drama")
+elif User_Input == 'Ecchi':
+    web.open ("https://aniworld.to/genre/ecchi")
+elif User_Input =='Engsub':
+    web.open ("https://aniworld.to/genre/engsub")
+elif User_Input == 'erotik':
+    web.open ("https://aniworld.to/genre/erotik")
+elif User_Input == 'fantasy':
+    web.open ("https://aniworld.to/genre/fantasy")
+elif User_Input == 'Fighting-shouen':
+    web.open ("https://aniworld.to/genre/fighting-shounen")
+elif User_Input == 'ganbatte':
+    web.open ("https://aniworld.to/genre/ganbatte")
+elif User_Input == 'Geistergeschichten':
+    web.open ("https://aniworld.to/genre/geistergeschichten")
+elif User_Input == 'ger':
+    web.open ("https://aniworld.to/genre/ger")
+elif User_Input == 'gersub':
+    web.open ("https://aniworld.to/genre/gersub")
+elif User_Input == 'harem':
+    web.open ("https://aniworld.to/genre/harem")
+elif User_Input == 'Horror':
+    web.open ("https://aniworld.to/genre/horror")
+elif User_Input == 'Komödie':
+    web.open ("https://aniworld.to/genre/komoedie")
+elif User_Input == 'krimi':
+    web.open ("https://aniworld.to/genre/komoedie")
+elif User_Input == 'Liebesdrama':
+    web.open ("https://aniworld.to/genre/liebesdrama")
+elif User_Input == 'Magical Girl':
+    web.open ("https://aniworld.to/genre/magical-girl")
+elif User_Input == 'Mecha':
+    web.open ("https://aniworld.to/genre/mecha")
+elif User_Input == 'mystery':
+    web.open ("https://aniworld.to/genre/mystery")
+elif User_Input == 'Nonsense-komödie':
+    web.open ("https://aniworld.to/genre/nonsense-komoedie")
+elif User_Input == 'psychodrama':
+    web.open ("https://aniworld.to/genre/psychodrama")
+elif User_Input == 'romantische-komoedie':
+    web.open ("https://aniworld.to/genre/romantische-komoedie")
+elif User_Input == 'Romanze':
+    web.open ("https://aniworld.to/genre/romanze")
+elif User_Input == 'scifi':
+    web.open ("https://aniworld.to/genre/scifi")
+elif User_Input == 'Sport':
+    web.open ("https://aniworld.to/genre/sport")
+elif User_Input == 'Thriller':
+    web.open ("https://aniworld.to/genre/thriller")
+elif User_Input == 'Yuri':
+    web.open ("https://aniworld.to/genre/yuri")
+elif User_Input == 'Gewalt':
+    web.open ("https://aniworld.to/genre/uebermaessige-gewaltdarstellung")
